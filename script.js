fa566ecfb51b0163aa762df3c0134726360d0616
AbHi <55599278+slim-python@users.noreply.github.com>
2020-05-02 19:11:33 +0530

    Add files via upload
    first time upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function isCheck(){
+    var num =                 document.getElementById('oddeven').value;
+     if ((num%2) ===0){
+
+        document.getElementById('msg').
+        innerHTML= num+" is an even number";  
+  }
+  else{
+
+    document.getElementById('msg').
+    innerHTML= num+" is an odd number";  
+
+    }
+
+}
